eb6cfd98f9611e74b9dc9bc57754e1932c50597e
junya takayama <nekonaderi@gmail.com>
2019-10-17 23:38:17 +0900

    フッター

---
+++ components/organism/GlobalFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <small>© 2019 Keiichiro Kimori</small>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="postcss" scoped>
+footer {
+  border-top: solid 1px var(--subColor);
+  margin-top: 16px;
+  padding: 12px 0;
+  text-align: center;
+}
+
+small {
+  color: var(--subColor);
+}
+</style>
