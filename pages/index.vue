36b07dadd41add5a5fb71506da5ad2bb86d4e509
junya takayama <nekonaderi@gmail.com>
2019-12-02 00:48:30 +0900

    TOPついk
    TOP追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,54 @@
 <template>
-  <main>
-    めいん
-  </main>
+  <MainWrapper>
+    <h1>
+      <img
+        src="~/assets/images/top/img-main.png"
+        srcset="
+          ~/assets/images/top/img-main.png    1x,
+          ~/assets/images/top/img-main@2x.png 2x
+        "
+        alt="Kimori Keiichiro ArtWork 肉体概念"
+      />
+    </h1>
+
+    <ContentsWrap class="top-contents">
+      <p class="main-lead">
+        自己紹介とこのサイトはこういうところ<br />
+        っていうのをエモくかいたりする<br />
+        自己紹介とこのサイトはこういうところっていうのをエモくかいたりする自己紹介とこのサイトはこういうところ<br />
+        っていうのをエモくかいたりする<br />
+        自己紹介とこのサイトはこういうところ<br />
+        っていうのをエモくかいたりする自己紹介とこのサイトはこういうところっていうのをエモくかいたりする
+      </p>
+
+      <section>
+        <HeadingBasic :tag="2" :level="1">Biography</HeadingBasic>
+      </section>
+    </ContentsWrap>
+  </MainWrapper>
 </template>
 
 <script lang="ts">
-export default {}
+import Vue from 'vue'
+import MainWrapper from '~/components/molecules/MainWrapper.vue'
+import ContentsWrap from '~/components/molecules/ContentsWrap.vue'
+import HeadingBasic from '~/components/atoms/HeadingBasic.vue'
+
+export default Vue.extend({
+  components: {
+    MainWrapper,
+    ContentsWrap,
+    HeadingBasic
+  }
+})
 </script>
 
 <style lang="postcss">
-main {
-  padding: 16px 0 70px;
+h1 img {
+  width: 100%;
+}
+
+.top-contents {
+  margin-top: 42px;
 }
 </style>
