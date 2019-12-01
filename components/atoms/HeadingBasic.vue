c53d5dab0a18f2426a66edbf2136c918fe2f9670
junya takayama <nekonaderi@gmail.com>
2019-12-02 00:47:44 +0900

    見出し

---
+++ components/atoms/HeadingBasic.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="headingTag" :class="headingClass"><slot /></component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    tag: {
+      type: String,
+      default: '1'
+    },
+    level: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    headingTag() {
+      return `h${this.tag}`
+    },
+    headingClass() {
+      return `heading-${this.level}`
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.heading-1 {
+  color: var(--mainColor);
+  font-size: var(--largeFontSize);
+  font-weight: normal;
+}
+</style>
