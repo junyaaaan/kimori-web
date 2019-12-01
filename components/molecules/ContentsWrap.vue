05c4f77999e36f599f116a5b1909081d9a3ce003
junya takayama <nekonaderi@gmail.com>
2019-12-02 00:48:12 +0900

    共通マージン用コンポーネント

---
+++ components/molecules/ContentsWrap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contents-wrap">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="postcss" scoped>
+.contents-wrap {
+  padding: 0 12px;
+}
+
+.contents-wrap > *:not(:first-child) {
+  margin-top: 42px;
+}
+</style>
---
+++ components/molecules/MainWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="postcss" scoped>
+main {
+  padding: 16px 0 70px;
+  max-width: 960px;
+  margin: 0 auto;
+}
+</style>
