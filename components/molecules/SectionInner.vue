44bb6e4cf8a7f3b38e5831169227e4b8f634e957
junya takayama <nekonaderi@gmail.com>
2019-12-10 00:23:43 +0900

    活動を追加

---
+++ components/molecules/SectionInner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section-inner"><slot /></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="postcss" scoped>
+.section-inner {
+  margin-top: 16px;
+}
+</style>
---
+++ pages/biography/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <MainWrapper>
+    <ContentsWrap>
+      <section>
+        <HeadingBasic :tag="1" :level="1">Biography</HeadingBasic>
+
+        <SectionInner>
+          <BiographyList :biographys="biographys" :omit="false" />
+        </SectionInner>
+      </section>
+    </ContentsWrap>
+  </MainWrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+import MainWrapper from '~/components/molecules/MainWrapper.vue'
+import ContentsWrap from '~/components/molecules/ContentsWrap.vue'
+import HeadingBasic from '~/components/atoms/HeadingBasic.vue'
+import SectionInner from '~/components/molecules/SectionInner.vue'
+import BiographyList from '~/components/organism/BiographyList.vue'
+
+export default Vue.extend({
+  components: {
+    MainWrapper,
+    ContentsWrap,
+    HeadingBasic,
+    SectionInner,
+    BiographyList
+  },
+  computed: {
+    ...mapGetters({
+      biographys: 'biography/all'
+    })
+  }
+})
+</script>
